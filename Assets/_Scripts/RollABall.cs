8f2bd580911ef7a12abed566e3a0f0695005f680
avalenci <ansmeew@gmail.com>
2020-10-26 19:26:45 -0400

    implemented timer

--- Assets/_Scripts/RollABall.cs
+++ Assets/_Scripts/RollABall.cs
@@ -1,16 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RollABall : MonoBehaviour
 {
     public GameObject[] boards;
+    public Text uiTime;
 
+    public double subTime;
+    public bool stopTimer = false;
     public int level;
     public GameObject board;
 
     void Start()
     {
+        subTime = 0;
         level = 0;
         StartLevel();
     }
@@ -31,6 +36,7 @@ public class RollABall : MonoBehaviour
         {
             Invoke("NextLevel", 2f);
             Goal.isWin = false;
+            stopTimer = true;
         }
 
         if (Hole.isLose)
@@ -38,11 +44,23 @@ public class RollABall : MonoBehaviour
             Invoke("StartLevel", 2f);
             Hole.isLose = false;
         }
+
+        if (!stopTimer)
+        {
+            UpdateGUI();
+        }
+    }
+
+    void UpdateGUI()
+    {
+        uiTime.text = "Time: " + (int)(Time.time - subTime);
     }
 
     void NextLevel()
     {
         level++;
+        stopTimer = false;
+        subTime = Time.time;
         StartLevel();
     }
 }
