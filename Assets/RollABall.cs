418b91dac9c14f38f133ebb69c50a3314106798d
avalenci <ansmeew@gmail.com>
2020-10-25 20:13:16 -0400

    implemented mutliple template levels

---
+++ Assets/RollABall.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RollABall : MonoBehaviour
+{
+    public GameObject[] boards;
+
+    public int level;
+    public GameObject board;
+
+    void Start()
+    {
+        level = 0;
+        StartLevel();
+    }
+
+    void StartLevel()
+    {
+        if (board != null)
+        {
+            Destroy(board);
+        }
+
+        board = Instantiate<GameObject>(boards[level]);
+    }
+
+    void Update()
+    {
+        if (Goal.isWin)
+        {
+            Invoke("NextLevel", 2f);
+            Goal.isWin = false;
+        }
+    }
+
+    void NextLevel()
+    {
+        level++;
+        StartLevel();
+    }
+}
