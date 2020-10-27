344171189c13ae0d047884cb9172392f96c8d10c
avalenci <ansmeew@gmail.com>
2020-10-27 15:02:54 -0400

    created scrolling camera (follows ball)

---
+++ Assets/_Scripts/ScrollingCam.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingCam : MonoBehaviour
+{
+    static public GameObject target;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        target = GameObject.Find("Ball");
+        transform.position = new Vector3(
+            Mathf.Lerp(transform.position.x, target.transform.position.x, 0.75f),
+            transform.position.y,
+            Mathf.Lerp(transform.position.z, target.transform.position.z, 0.75f));
+    }
+}
